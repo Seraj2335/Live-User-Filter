21f95be94b13155c720643f1adc5416190bb73c2
Seraj ahmad <kseraj96@gmail.com>
2022-04-02 09:47:11 +0530

    commit

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const result = document.getElementById('result')
-const filter = document.getElementById('filter')
-const listItems = []
+const result = document.getElementById('result');
+const filter = document.getElementById('filter');
+const listItems = [];
 
-getData()
+getData();
 
-filter.addEventListener('input', (e) => filterData(e.target.value))
+filter.addEventListener('input', (e) => filterData(e.target.value));
 
 async function getData() {
-    const res = await fetch('https://randomuser.me/api?results=50')
+    const res = await fetch('https://randomuser.me/api?results=50');
 
-    const { results } = await res.json()
+    const { results } = await res.json();
 
     // Clear result
-    result.innerHTML = ''
+    result.innerHTML = '';
 
     results.forEach(user => {
-        const li = document.createElement('li')
+        const li = document.createElement('li');
 
-        listItems.push(li)
+        listItems.push(li);
 
         li.innerHTML = `
             <img src="${user.picture.large}" alt="${user.name.first}">
@@ -25,9 +25,9 @@ async function getData() {
                 <h4>${user.name.first} ${user.name.last}</h4>
                 <p>${user.location.city}, ${user.location.country}</p>
             </div>
-        `
+        `;
 
-        result.appendChild(li)
+        result.appendChild(li);
     })
 }
 
